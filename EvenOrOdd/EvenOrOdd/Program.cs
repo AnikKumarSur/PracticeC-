173847c3a0c91a7c75c1c03058a1dc7d9137c6e9
DeveloperAnik <53524330+DeveloperAnik@users.noreply.github.com>
2024-05-10 15:47:55 +0600

    Even or Odd number

---
+++ EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Test data : ");
+            int testData = Convert.ToInt32(Console.ReadLine());
+
+            if(testData%2==0)
+            {
+                Console.WriteLine("{0} The given number is even integer." , testData);
+            }
+            else
+            {
+                Console.WriteLine("{0} The given number is odd." , testData);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
