3c43ab8030a60fdae0de9e5451bfe7f8308a03bc
DeveloperAnik <53524330+DeveloperAnik@users.noreply.github.com>
2024-05-10 15:37:19 +0600

    Equal are not

---
+++ FirstConditionalApp/FirstConditionalApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConditionalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first Number : ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input Second Number : ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if(firstNumber == secondNumber)
+            {
+                Console.WriteLine("{0} and {1} are equal . ", firstNumber, secondNumber);
+            }
+            else
+            {
+                Console.WriteLine("{0} and {1} are not equal.", firstNumber , secondNumber );
+            }
+            Console.ReadKey();
+        }
+    }
+}
